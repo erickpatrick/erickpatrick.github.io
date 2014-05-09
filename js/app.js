4afee19c05fbc39e79e68645d9022b3d665b5efb
Erick Patrick <programming@erickpatrick.com>
2014-05-09 15:46:54 -0300

    Adjust header to screen without JavaScript
    Removed JavaScript dependency and started using *vh* units and % to
    header element

--- js/app.js
+++ js/app.js
@@ -4,25 +4,5 @@ yepnope([{
     if (!window.jQuery) {
       yepnope('js/jquery.min.js');
     }
-
-    var header = $('.header'),
-    	resizeHeader = function() {
-    		var screenWidth = $(window).width(),
-    			screenHeight = $(window).height();
-
-    		// setting header to screen's height and width
-		    header.width(screenWidth);
-		    header.height(screenHeight);
-    	};
-
-    window.scrollTo(0,1);
-    resizeHeader();
-
-    setInterval(resizeHeader, 100);
-
-    $('document').on('resize', function() {
-    	resizeHeader();
-    });
-    
   }
 }]);
