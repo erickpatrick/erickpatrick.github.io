2b39e11783c1d19e63904c6436ccb77483f0cad8
Erick Patrick <programming@erickpatrick.com>
2014-05-09 16:57:48 -0300

    Bootstraping all the things
    Started using Bootstrap.
    
    TODO
    - change heating old logo by my own

--- js/app.js
+++
@@ -1,8 +0,0 @@
-yepnope([{
-  load: '//ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js',
-  complete: function () {
-    if (!window.jQuery) {
-      yepnope('js/jquery.min.js');
-    }
-  }
-}]);
