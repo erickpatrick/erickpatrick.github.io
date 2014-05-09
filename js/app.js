b545cafa00c41e5e4adb7010309367c02d120ae4
Erick Patrick <programming@erickpatrick.com>
2014-05-09 15:23:51 -0300

    Menu Removal and Auto-adjust to screen
    Removed menu link and added script to adjust above the fold to the
    screen

--- js/app.js
+++ js/app.js
@@ -4,5 +4,24 @@ yepnope([{
     if (!window.jQuery) {
       yepnope('js/jquery.min.js');
     }
+
+    var header = $('.header'),
+    	resizeHeader = function() {
+    		var screenWidth = $(window).width(),
+    			screenHeight = $(window).height();
+
+    		// setting header to screen's height and width
+		    header.width(screenWidth);
+		    header.height(screenHeight);
+    	};
+
+    resizeHeader();
+
+    setInterval(resizeHeader, 100);
+
+    $('document').on('resize', function() {
+    	resizeHeader();
+    });
+    
   }
 }]);
