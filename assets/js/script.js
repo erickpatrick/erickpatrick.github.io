f3b9043d6c9e34d01f841804016bc36af7b13cf4
Erick Patrick <programming@erickpatrick.com>
2014-06-15 16:49:47 -0300

    Added JS interactions to cta

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,21 @@
-jQuery(document).ready(function(){
+$(document).ready(function(){
+  var overlay = $('.overlay'),
+      cta = $('#cta-contact'),
+      close = $('.overlay-close');
+
   // Adapt .box-main to the size of window
-  jQuery('html').css({height:'auto'});
-  jQuery('.box-main').css({minHeight:jQuery(window).height()});
-  jQuery(window).on('resize',function(){
-    jQuery('.box-main').css({minHeight:jQuery(window).height()});
+  $('html').css({height:'auto'});
+  $('.box-main').css({minHeight:$(window).height()});
+  $(window).on('resize',function(){
+    $('.box-main').css({minHeight:$(window).height()});
   });
+
+  cta.on('click', function(e) {
+    e.preventDefault();
+    overlay.toggleClass('open');
+  });
+  close.on('click', function(e) {
+    e.preventDefault();
+    overlay.toggleClass('open');
+  })
 });
