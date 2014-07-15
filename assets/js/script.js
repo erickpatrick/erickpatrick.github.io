e1586e522b4937e2bce763e380d993df7aabcaea
Erick Patrick <programming@erickpatrick.com>
2014-07-15 20:59:02 -0300

    Ajusted CSS
    Refactored code to be mobile first and work on smartphones e tablets

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,6 @@ $(document).ready(function(){
       cta = $('#cta-contact'),
       close = $('.overlay-close');
 
-  // Adapt .box-main to the size of window
-  $('html').css({height:'auto'});
-  $('.box-main').css({minHeight:$(window).height()});
-  $(window).on('resize',function(){
-    $('.box-main').css({minHeight:$(window).height()});
-  });
-
   cta.on('click', function(e) {
     e.preventDefault();
     overlay.toggleClass('open');
