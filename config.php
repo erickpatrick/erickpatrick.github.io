f5bec18e5a294dae3dd670a68ac688b183770d8b
Erick Patrick <contato@erickpatrick.net>
2017-03-18 22:58:51 +0100

    feat (head|config): added basic head content for master layout
    added basic head content for master layout using site wide data coming
    from config.php

--- config.php
+++ config.php
@@ -2,4 +2,24 @@
 
 return [
     'production' => false,
+    'bla' => 'erick',
+    'site' => [
+        'domain' => 'https://www.erickpatrick.net/',
+        'lang' => 'en',
+        'author' => 'Erick Patrick',
+        'title' => "Erick Patrick's Website",
+        'description' => 'Personal website from Brazilian web developer Erick Patrick containing various projects.',
+        'thumbnail' => 'https://www.erickpatrick.net/img/thumbnail.jpg',
+        'social' => [
+            'twitter' => '@erickpatrick',
+            'facebook' => [
+                'fbadmin' => '1386933601',
+                'page' => 'https://fb.me/rpgdomestre'
+            ],
+            'gplus' => [
+                'author' => 'https://google.com/+ErickPatrick1988',
+                'publisher' => 'https://plus.google.com/b/106544148188542649191/106544148188542649191/posts'
+            ]
+        ]
+    ]
 ];
--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,10 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="@yield('lang', $page->site->lang)" itemscope itemtype="https://schema.org/Person">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <link rel="stylesheet" href="/css/main.css">
+        <meta name=viewport content="width=device-width, initial-scale=1">
+        <title>@yield('page-title', 'Homepage') @ {{ $page->site->title }}</title>
+        <meta name="description" content="@yield('description', $page->site->description)">
+        <meta name="author" content="{{ $page->site->author }}">
+
+        <!-- Twitter card -->
+        <meta name="twitter:card" content="summary">
+        <meta name="twitter:site" content="{{ $page->site->social->twitter }}">
+        <meta name="twitter:title" content="@yield('page-title', 'Homepage') @ {{ $page->site->title }}">
+        <meta name="twitter:description" content="@yield('description', $page->site->description)">
+        <meta name="twitter:creator" content="{{ $page->site->social->twitter }}">
+        <meta name="twitter:image:src" content="{{ $page->site->thumbnail }}">
+        <meta name="twitter:domain" content="{{ $page->site->domain }}">
+
+        <!-- Facebook Meta Tags -->
+        <meta property="og:url" content="{{ $page->site->domain }}"/>
+        <meta property="og:title" content="@yield('page-title', 'Homepage') @ {{ $page->site->title }}"/>
+        <meta property="og:image" content="{{ $page->site->thumbnail }}"/>
+        <meta property="og:site_name" content="{{ $page->site->title }}"/>
+        <meta property="og:description" content="{{ $page->site->description }}"/>
+
+        <!-- FB Insights -->
+        <meta property="fb:admins" content="{{ $page->site->social->facebook->fbadmin }}" />
+
+        <!-- Google+ Meta Tags -->
+        <meta itemprop="name" content="@yield('page-title', 'Homepage') @ {{ $page->site->title }}">
+        <meta itemprop="description" content="{{ $page->site->description }}">
+        <meta itemprop="image" content="{{ $page->site->thumbnail }}">
+
+        <link rel="author" href="{{ $page->site->social->gplus->author }}" />
+        <link rel="publisher" href="{{ $page->site->social->gplus->publisher }}">
     </head>
     <body>
         @yield('body')
